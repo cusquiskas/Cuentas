30534468ff288ef8e53d3d081b0ed30455f31202
cusquiskas <cusquiskas@gmail.com>
2019-05-18 17:14:54 +0200

    añadido el año 2020 en las estadísticas de consulta y añadido hasta el 2022 en la consulta de movimientos

--- data/menu/estadistica.php
+++ data/menu/estadistica.php
@@ -13,6 +13,7 @@
          <option value="2017" <?php if ($Estadistica->getAño() == '2017') echo 'selected'; ?>>2017</option>
          <option value="2018" <?php if ($Estadistica->getAño() == '2018') echo 'selected'; ?>>2018</option>
          <option value="2019" <?php if ($Estadistica->getAño() == '2019') echo 'selected'; ?>>2019</option>
+         <option value="2020" <?php if ($Estadistica->getAño() == '2020') echo 'selected'; ?>>2020</option>
         </select>
       </p>
       <br>
--- data/menu/totales.php
+++ data/menu/totales.php
@@ -33,6 +33,9 @@
          <option value="2017" <?php if ($Buscar->getAño() == '2017') echo 'selected'; ?>>2017</option>
          <option value="2018" <?php if ($Buscar->getAño() == '2018') echo 'selected'; ?>>2018</option>
          <option value="2019" <?php if ($Buscar->getAño() == '2019') echo 'selected'; ?>>2019</option>
+         <option value="2020" <?php if ($Buscar->getAño() == '2020') echo 'selected'; ?>>2020</option>
+         <option value="2021" <?php if ($Buscar->getAño() == '2021') echo 'selected'; ?>>2021</option>
+         <option value="2022" <?php if ($Buscar->getAño() == '2022') echo 'selected'; ?>>2022</option>
         </select>
 
 </p>
