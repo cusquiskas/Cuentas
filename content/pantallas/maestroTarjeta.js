f95e2904ceea9f14fe5f2aa1d2114659859a012c
CusQuisKas <cusquiskas@gmail.com>
2021-12-11 00:49:16 +0100

    el select con la lista de tarjetas, ahora lo dejo limpio

--- content/pantallas/maestroTarjeta.js
+++ content/pantallas/maestroTarjeta.js
@@ -11,6 +11,7 @@ var maestroTarjeta = class {
         let me = this;
         $('select[name=id]').ajaxSuccess(function (eve) {
             me.tarjetas = JSON.parse(eve.target.getAttribute('obj'));
+            eve.target.removeAttribute('obj');
         });
         $('select[name=id]').change     (function (eve) {
             let objeto = JSON.parse(JSON.stringify(me.tarjetas.find( opciones => opciones.id == eve.target.value)));
