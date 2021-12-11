a4d7ddf57422e17f8466960f2c4b3d66c6b2b2b0
José Miguel Munar <thrawn@MacBook-Air-de-Jose.local>
2021-12-11 18:29:55 +0100

    ya funcionan los botones, ya está el formulario bien armado y sólo falta que guarde en la base de datos

---
+++ backend/tarjeta/guardaTarjeta.php
@@ -0,0 +1,32 @@
+<?php
+
+    session_start();
+    error_reporting(E_ALL & ~E_NOTICE);
+
+    header('Content-Type: application/json; charset=utf-8');
+
+    if (!isset($_SESSION['data']) || !isset($_SESSION['data']['user']) || !isset($_SESSION['data']['user']['id']) || $_SESSION['data']['user']['id'] == '') {
+        die(json_encode(['success' => false, 'root' => [['tipo' => 'Sesion', 'Detalle' => 'Sesión no válida']]]));
+    }
+
+    require_once '../../conex/conf.php';  //información crítica del sistema
+    require_once '../../conex/dao.php';   //control de comunicación con la base de datos MySQL
+    require_once '../../tabla/controller.php';
+
+    $manTarjeta = ControladorDinamicoTabla::set('visa');
+
+    $registro = $_POST;
+    $registro['usuario'] = $_SESSION['data']['user']['id'];
+
+    if ($manTarjeta->save($registro)) {
+        echo json_encode(['success' => true, 'root' => ['tipo' => 'Respuesta', 'Detalle' => 'Registro modificado correctamente']]);
+    } else {
+        $reg = $manTarjeta->getListaErrores();
+        echo json_encode(['success' => false, 'root' => ['tipo' => 'Respuesta', 'Detalle' => $reg]]);
+    }
+    
+    unset($_POST);
+    unset($registro);
+    unset($reg);
+    unset($manTarjeta);
+?>
--- content/pantallas/maestroTarjeta.js
+++ content/pantallas/maestroTarjeta.js
@@ -9,15 +9,34 @@ var maestroTarjeta = class {
 
     addEventos() {
         let me = this;
+        $('button[name=limpiar]').click(function () {
+            let select = $('select[name=id]');
+            select[0].value = "";
+            select.trigger('change');
+        });
         $('select[name=id]').ajaxSuccess(function (eve) {
+            let option=document.createElement("option");
+            option.value=""
+            option.text=".. Selecciona una tarjeta ..";
             me.tarjetas = JSON.parse(eve.target.getAttribute('obj'));
+            me.tarjetas[me.tarjetas.length] = {"id":"","descripcion":"","fila":"","c_fecha":"","c_descripcion":"","c_importe":"","c_concepto1":"","c_concepto2":"","c_concepto3":"","c_concepto4":"","d_corte":"","d_recordatorio":"","c_separador_d":"","c_separador_c":"","mascara":"","inv_import":"N"};
             eve.target.removeAttribute('obj');
+            eve.target.appendChild(option);
+            eve.target.value = "";
         });
         $('select[name=id]').change     (function (eve) {
             let objeto = JSON.parse(JSON.stringify(me.tarjetas.find( opciones => opciones.id == eve.target.value)));
             delete objeto.id;
             me.modulo.Forms['frmTarjeta'].set(objeto);
         });
+    };
+
+    guardar (s, d, e) {
+        if (s) {
+            debugger
+        } else {
+            validaErroresCBK(d.root||d);
+        }
     };
 
 }
