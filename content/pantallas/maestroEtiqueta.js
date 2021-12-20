5cfc9c49fea7764316bbc65c63b30bcec7c8c116
CusQuisKas <cusquiskas@gmail.com>
2021-12-20 23:11:58 +0100

    Empezamos con las etiquetas y ya de paso solucionamos un error del controler.php

--- content/pantallas/maestroEtiqueta.js
+++ content/pantallas/maestroEtiqueta.js
@@ -2,4 +2,8 @@ var maestroEtiqueta = class {
     constructor (mod, obj) {
         console.log('maestroEtiqueta.js -> constructor');
     };
+
+    listar (s,d,e) {
+        console.log('maestroEtiqueta.js -> listado de Etiquetas');
+    }
 }
