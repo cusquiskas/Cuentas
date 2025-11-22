4d2fe3ce46705213aba8d292a42121fd2d54970d
Jose Miguel <jmmunar@cusquiskas.com>
2025-11-22 20:14:23 +0100

    liberado el archivo de configuración, ya no guardo contraseñas en los fuentes

--- Dockerfile
+++
@@ -1,4 +0,0 @@
-FROM php:7.0-apache
-
-# Instalar extensiones necesarias para bases de datos
-RUN docker-php-ext-install mysqli pdo pdo_mysql
--- MVC/modelo/configuracion.php
+++ MVC/modelo/configuracion.php
@@ -14,6 +14,24 @@ class configuracion {
    public function getApli() { return $this->apli; }
    
    public function getHome() { return $this->home; }
+
+   public function __construct   () {
+        if (file_exists('/opt/lampp/htdocs/claves.json')) {
+            $config = json_decode(file_get_contents('/opt/lampp/htdocs/claves.json'), true);
+            $config = $config["Cuentas"];
+            
+            $this->host        = $config["host"       ];
+            $this->user        = $config["user"       ];
+            $this->pass        = $config["pass"       ];
+            $this->apli        = $config["apli"       ];
+
+            $this->home        = $config["home"       ];
+
+        } elseif (getenv('DB_HOST')) {
+            echo "estoy en excelsiu\n";
+        }
+    
+    }
 }
 
 $modeloConfiguracion = new configuracion();
